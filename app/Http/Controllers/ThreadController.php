b3286675ac15fc55fdeebd98610e55f9c2a1e4c6
veronj <veron_j@etna-alternance.net>
2018-11-07 21:39:14 +0100

    thread index

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -14,7 +14,9 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        //
+        $threads = Thread::latest()->get();
+
+        return view('threads.index', compact('threads'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/threads', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadController@index');
