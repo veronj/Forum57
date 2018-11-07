35d067e5eeb13ff67bf4df586a7496bd5c7c6e80
veronj <veron_j@etna-alternance.net>
2018-11-07 19:11:15 +0100

    test 1

--- tests/Feature/TheadsTest.php
+++ tests/Feature/TheadsTest.php
@@ -4,18 +4,22 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
+    use DatabaseMigrations;
+/** @test  */
     public function a_user_can_browse_threads()
     {
         $response = $this->get('/threads');
 
         $response->assertStatus(200);
+    }
+/** @test  */
+    public function this_test_()
+    {
+        
     }
 }
